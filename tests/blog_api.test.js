2a839cbbe4eaecefcab1b52ed0d46e97d2981fc6
Daliya <daliyadeutsch@gmail.com>
2025-03-02 23:28:54 +0530

    ex 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,11 @@
-const { test, after, beforeEach } = require("node:test");
-const assert = require("assert"); // Import assert
+const { describe, test, beforeEach, after } = require("node:test");
+const assert = require("assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-const Blog = require("../models/blog"); // Corrected model import
+const Blog = require("../models/blog");
 
-const api = supertest(app); // Define api before tests
+const api = supertest(app);
 
 const initialBlogs = [
   {
@@ -22,32 +22,50 @@ const initialBlogs = [
   },
 ];
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-
-  let blogObject = new Blog(initialBlogs[0]);
-  await blogObject.save();
-
-  blogObject = new Blog(initialBlogs[1]);
-  await blogObject.save();
-});
-
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("blog posts have an 'id' property instead of '_id'", async () => {
-  const response = await api.get("/api/blogs");
-
-  response.body.forEach((blog) => {
-    assert(blog.id, "Blog post should have an 'id' property");
-    assert(!blog._id, "Blog post should not have an '_id' property");
+describe("Blog API tests", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(initialBlogs);
   });
-});
 
-after(async () => {
-  await mongoose.connection.close();
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("blog posts have an 'id' property instead of '_id'", async () => {
+    const response = await api.get("/api/blogs");
+
+    response.body.forEach((blog) => {
+      assert(blog.id, "Blog post should have an 'id' property");
+      assert(!blog._id, "Blog post should not have an '_id' property");
+    });
+  });
+
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Mastering Async/Await in JavaScript",
+      author: "Jane Doe",
+      url: "https://example.com/async-await",
+      likes: 15,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map((b) => b.title);
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1);
+    assert(titles.includes("Mastering Async/Await in JavaScript"));
+  });
+
+  after(async () => {
+    await mongoose.connection.close();
+  });
 });
