a9321008ba329cc16ed42bcf4696b4e094b0df0f
Dhaliya Noushad <daliyadeutsch@gmail.com>
2025-03-04 01:27:20 +0530

    ex 4.18

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-const config = require("./utils/config");
 const express = require("express");
 require("express-async-errors");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Import utility modules
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+
+// Import route controllers
+const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
+// Initialize Express app
+const app = express();
+
+// Configure Mongoose
 mongoose.set("strictQuery", false);
-
-logger.info("connecting to MongoDB");
+logger.info("Connecting to MongoDB...");
 
 mongoose
   .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
-  });
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) =>
+    logger.error("Error connecting to MongoDB:", error.message)
+  );
 
+// Middleware setup
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+// Route setup
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
+// Error handling middleware
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
+// Export app module
 module.exports = app;
