24406fa583852ff7863bbbcec5529ef1e885c7c0
Daliya <daliyadeutsch@gmail.com>
2025-03-03 16:40:44 +0530

    ex 4.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,19 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((result) => {
-    response.json(result);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) {
-        response.json(blog);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+
+  if (!blog) {
+    return response.status(404).json({ error: "Blog not found" });
+  }
+
+  response.json(blog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
@@ -29,29 +26,19 @@ blogsRouter.delete("/:id", async (request, response) => {
   response.status(204).end();
 });
 
-blogsRouter.post("/", (request, response, next) => {
-  const body = request.body;
+blogsRouter.post("/", async (request, response) => {
+  const { title, author, url, likes } = request.body;
 
-  if (!body.title || !body.url || !body.author) {
+  if (!title || !url || !author) {
     return response.status(400).json({ error: "Title, URL or Author missing" });
   }
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  });
+  const blog = new Blog({ title, author, url, likes });
+  const savedBlog = await blog.save();
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog);
-    })
-    .catch((error) => next(error));
+  response.status(201).json(savedBlog);
 });
 
-// New PUT endpoint to handle updates
 blogsRouter.put("/:id", async (request, response) => {
   const { title, author, url, likes } = request.body;
 
