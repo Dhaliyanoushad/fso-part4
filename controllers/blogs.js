147c3894c656406c3fe74b8487012bcf8491c4ac
Dhaliya Noushad <daliyadeutsch@gmail.com>
2025-03-04 13:42:56 +0530

    ex 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 }); // Populate user details
 
@@ -27,19 +19,9 @@ blogsRouter.get("/:id", async (request, response) => {
   response.json(blog);
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const deletedBlog = await Blog.findByIdAndDelete(request.params.id);
-
-  if (!deletedBlog) {
-    return response.status(404).json({ error: "Blog not found" });
-  }
-
-  response.status(204).end();
-});
-
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid" });
   }
@@ -56,11 +38,22 @@ blogsRouter.post("/", async (request, response) => {
     likes: body.likes || 0,
     user: user.id,
   });
+
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(savedBlog._id);
   await user.save();
 
   response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  const deletedBlog = await Blog.findByIdAndDelete(request.params.id);
+
+  if (!deletedBlog) {
+    return response.status(404).json({ error: "Blog not found" });
+  }
+
+  response.status(204).end();
 });
 
 blogsRouter.put("/:id", async (request, response) => {
